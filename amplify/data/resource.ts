e6da58aaf8d5e08d4915805044ba9987c4b7dba9
Ringo-Leung <ringo.leung@forcegator.com>
2024-07-31 01:00:15 +0800

    Bug Fix for auth

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,10 +1,20 @@
-import { defineStorage } from "@aws-amplify/backend";
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-export const storage = defineStorage({
-  name: "amplifyNotesDrive",
-  access: (allow) => ({
-    "media/{entity_id}/*": [
-      allow.entity("identity").to(["read", "write", "delete"]),
-    ],
-  }),
+const schema = a.schema({
+  Note: a
+    .model({
+      name:a.string(),
+      description: a.string(),
+      image: a.string(),
+    })
+    .authorization((allow) => [allow.owner()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'userPool',
+  },
 });
